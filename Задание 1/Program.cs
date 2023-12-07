f1cdf45b328ab1bf09c42a64794c7c5312948d55
Карякина Настя <nakarykina04@mail.ru>
2023-12-08 01:34:35 +0300

    добавила решение задание 1

--- Задание 1/Program.cs
+++ Задание 1/Program.cs
@@ -1,13 +1,9 @@
-﻿// Задайте одномерный массив из 10 целых чисел от 1 
-//до 100. Найдите количество элементов массива, 
-//значения которых лежат в отрезке [20,90].
+﻿// Задайте одномерный массив из 10 целых чисел от 1 до 100. Найдите количество элементов массива, значения которых лежат в отрезке [20,90].
 
-using System.Formats.Asn1;
-
-System.Console.Write("Введите количество элементов в массиве: ");
+System.Console.WriteLine("Введите количество элементов в массиве: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-int [] array = new int [number];
+int[] array = new int [number];
 Random r = new Random();
 
 int count = 0;
@@ -19,10 +15,8 @@ for ( int i = 0; i < number; i++)
 System.Console.WriteLine("Сгенерироваемый массив:");
 for ( int i = 0; i < number; i++)
 {
-    
     System.Console.Write(array[i] + " ");
 }
-
 for ( int i = 0; i < number; i++)
 {
     if (array[i] >= 20 && array[i] <= 90)
@@ -30,4 +24,4 @@ for ( int i = 0; i < number; i++)
         count++;
     }
 }
-System.Console.WriteLine($"Количество элементов массива, значения которых лежат в интервале [20, 90]: {count}");
+System.Console.WriteLine($" В данном массиве количество элементов в диапозоне [20; 90] = {count}");
